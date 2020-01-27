3550d79c2f4f0e7dd807c8fea649713228fc748d
DarkCrow3596 <60354937+DarkCrow3596@users.noreply.github.com>
2020-01-27 22:37:53 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FindMed</title>
+        <link rel="stylesheet" type="text/css" href="All.css" />
+    </head>
+    <body bgcolor=#F2D0D0>
+        <center>
+        <div>
+            <table width=100%>
+                <tr bgcolor=#D9A0B5><td align=center><h1 class="heading heading--stroke heading--shadow">FindMed</h1></td></tr>
+                <tr bgcolor=#F2F2DF><td align=center pading=0px><font color=#A63875>Find your hospital here on our website</font></td></tr>
+            </table>
+            <p></p>
+        </div>
+        <div>
+            <p></p>
+            <a href="https://rikku.000webhostapp.com/gis/news.php" class="action-button shadow animate yellow">News</a>
+            <a href="https://rikku.000webhostapp.com/gis/search.php" class="action-button shadow animate yellow">Search</a>
+            <a href="https://rikku.000webhostapp.com/gis/maps.php" class="action-button shadow animate yellow">Maps</a>
+            <a href="https://rikku.000webhostapp.com/gis/about.html" class="action-button shadow animate yellow">AboutUs</a>
+            <p></p>
+        </div>
+        <div class="about">
+            <table width=80%>
+            <tr align=center bgcolor=#F2F2DF>
+                <td width=40%>
+                    <br><font size="6">Welcome</font>
+                    <p align=justify>Our Website provide information for people that need information about hospital in North Sumatra Indonesia, so it can help many peoples to find a hospital near them</p>
+                <p><img src="https://rikku.000webhostapp.com/gis/images/hospital.jpg" alt="Hospital" height="250" weight="150"></p></td>
+                <td width=40% valign="top">No News Available</td>
+            </tr>
+        </table></div>
+        </center>
+    </body>
+</html>
